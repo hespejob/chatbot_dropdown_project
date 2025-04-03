5c393fc64426855d68272911dfc06d5a52912c9a
Ing. CIP Hugo Espejo <41070497+hespejob@users.noreply.github.com>
2025-04-03 10:21:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -116,6 +116,6 @@ def webhook():
             print("Webhook error:", e)
         return "OK", 200
 
-if _name_ == '_main_':
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
